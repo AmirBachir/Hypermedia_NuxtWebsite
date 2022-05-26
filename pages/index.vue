f58d00244477c480d123cbd7fcc9dd537f4d31ca
AmirBachir <amir.bachir@mail.polimi.it>
2022-05-26 20:02:13 +0200

    fixed intro in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="homepage">
-    <img src="~/assets/homepage-img.jpeg" alt="homepage image"/>
+    <img src="~/assets/homepage-img.jpeg" alt="homepage image" />
     <svg
       class="arrow-down"
       width="79"
@@ -8,10 +8,10 @@
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M2 3L39.5 30L77 3" stroke="#D8EFF5" stroke-width="5"/>
+      <path d="M2 3L39.5 30L77 3" stroke="#D8EFF5" stroke-width="5" />
     </svg>
-    <br/>
-    <table id="brescia-intro">
+    <br />
+    <!-- <table id="brescia-intro">
       <tr>
         <td>
           <h2>Brescia, The Lion Queen of Italy</h2>
@@ -31,11 +31,70 @@
           <img src="~/assets/intro_brescia.png" alt="intro brescia"/>
         </td>
       </tr>
-    </table>
+    </table> -->
+    <!-- <div class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="col">
+        <div class="card h-100">
+          <div class="card-body">
+            <h3 class="card-title">Brescia, The Lion Queen of Italy</h3>
+            <p class="card-text">
+              Lombardy’s second-largest city (Milan is first) is respected for
+              its metallurgy and machine-tooling prowess and for Beretta, its
+              firearms manufacturer. <br />
+              But the city center is a UNESCO World Heritage site in Italy and a
+              3,200-year-old history you can experience. Brescia offers tourists
+              everything they might want in the quest for dolce vita – food,
+              wine, shopping, sites, culture, and antiquity.
+            </p>
+            <a href="/the-city-brescia" class="btn">Discover</a>
+          </div>
+        </div>
+      </div>
+      <div class="col" style="height:300px">
+        <div class="card h-100" >
+          <img
+            src="~/assets/intro_brescia.png"
+            class="card-img-top"
+            alt="intro brescia"
+            style="height:inherit"
+          />
+        </div>
+      </div>
+    </div> -->
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 ">
+            <h3 class="">Brescia, The Lion Queen of Italy</h3>
+            <p class="">
+              Lombardy’s second-largest city (Milan is first) is respected for
+              its metallurgy and machine-tooling prowess and for Beretta, its
+              firearms manufacturer. <br />
+              But the city center is a UNESCO World Heritage site in Italy and a
+              3,200-year-old history you can experience. Brescia offers tourists
+              everything they might want in the quest for dolce vita – food,
+              wine, shopping, sites, culture, and antiquity.
+            </p>
+            <div class="text-center">
+            <a href="/the-city-brescia" class="btn">Discover</a>
+            </div>
+          </div>
+        <div class="col-sm-6 text-center">
+            <img
+              src="~/assets/intro_brescia.png"
+              class=""
+              alt="intro brescia"
+              style="height:400px"/>
+        </div>
+      </div>
+    </div>
+
+
+
     <h1>What to do in Brescia</h1>
-    <table id="what-to-do">
+    <table id="what-to-do" class="sticky-top">
       <tr class="container">
-        <td v-for="(e,k) of topics" :key="k">
+        <td v-for="(e, k) of topics" :key="k">
           <topic-card
             :title="e.title"
             :img-path="e.img_path"
@@ -71,10 +130,10 @@
     </table>
     <h1>Brescia waits for you!</h1>
     <p>
-      For more information please contact <br/><a
-      href="mailto:hello@brescia.it"
-    >hello@brescia.it</a
-    >
+      For more information please contact <br /><a
+        href="mailto:hello@brescia.it"
+        >hello@brescia.it</a
+      >
     </p>
   </div>
 </template>
@@ -83,8 +142,8 @@
 // import CustomPage from '~/components/CustomPage.vue'
 export default {
   name: 'IndexPage',
-  async asyncData({$axios}) {
-    const {data} = await $axios.get('/api/page-info/index')
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get('/api/page-info/index')
     const title = data.title
     const image = data.image
     const description = data.description
@@ -106,19 +165,22 @@ export default {
         {
           title: 'Events',
           img_path: 'brescia_eve.jpg',
-          description: 'Enjoy the city with some of the most exciting events on sight.',
+          description:
+            'Enjoy the city with some of the most exciting events on sight.',
           url: '/events',
         },
         {
           title: 'Itineraries',
           img_path: 'brescia_iti.jpg',
-          description: 'Plan your visit with our custom recommendations on tours, walks and what to see.',
+          description:
+            'Plan your visit with our custom recommendations on tours, walks and what to see.',
           url: '/itineraries',
         },
         {
           title: 'Points of Interest',
           img_path: 'brescia_poi.jpg',
-          description: 'Discover all of the magical spots that Brescia has to offer to the curious traveler.',
+          description:
+            'Discover all of the magical spots that Brescia has to offer to the curious traveler.',
           url: '/points-of-interest-intro',
         },
       ],
@@ -135,6 +197,7 @@ export default {
 
 img {
   max-width: 100%;
+  /* height:300px; */
 }
 
 h1 {
@@ -144,15 +207,19 @@ h1 {
   margin-bottom: 50px;
 }
 
+.row p{
+  text-align: left;
+}
+
 p {
   font-family: Inter, serif;
   font-weight: 100;
   text-align: center;
 }
 
-button {
-  display: block;
-  margin: auto;
+.btn {
+  /* display: block;
+  margin: auto; */
   padding: 10px;
   border-style: solid;
   border-width: 1px;
@@ -162,7 +229,7 @@ button {
   border-color: #d8eff5;
 }
 
-button:hover {
+.btn:hover {
   background: #d8eff5;
   color: #234c60;
   -webkit-transition: all 0.5s;
@@ -183,6 +250,7 @@ a {
 
 #brescia-intro {
   overflow-x: auto;
+  width: 100%;
 }
 
 #brescia-intro td {
